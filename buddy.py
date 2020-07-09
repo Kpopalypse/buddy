6422972be647f0455c181edbc66a97a42cd5edbc
Kpopalypse <59384765+Kpopalypse@users.noreply.github.com>
2020-07-09 12:55:22 +0930

    Update buddy.py
    * Added better program notes
    * Adjustable sleep time
    * Now correctly breaks loop if process match found
    * Variables now use boolean values where applicable

--- buddy.py
+++ buddy.py
@@ -1,33 +1,39 @@
 import psutil, os, time, traceback, getpass
 
-found = 0
+found = False
+sleeptime = 10                                           # amount of time the program waits between reads
 
-theprogram = 'connecttimer.exe'
+theprogram = 'connecttimer.exe'                          # the name of the process you want to check
 
-the_user = getpass.getuser()
+the_user = getpass.getuser()                             # finds out the windows user profile name
 
 try:
 
     while True:
 
         for process in psutil.process_iter():
-            if process.name() == theprogram:
-                found = 1
-                print ('process found')
+            try:
+                if process.name() == theprogram:         # breaks the loop if the program is found
+                    found = True
+                    print ('process found')
+                    break
+            except:
+                print ('error')                          # tries again in a minute if another program interferes with the process
+                time.sleep(100)
+                continue
                 
-
-        if found == 0:
-            print ('process not found')
+        if found == False:
+            print ('process not found')                  # starts the program
             filepath = theprogram
             os.startfile(filepath)
             
-        found = 0
+        found = False
 
-        time.sleep(10)
+        time.sleep(sleeptime)
         print ('sleeping')
         continue
 
-except:
+except:                                                  # prints traceback to a text file for debugging if there is a crash
     os.chdir("C://Users/" + the_user + "/desktop")
     with open("buddyerror.log", "a") as logfile:
         traceback.print_exc(file=logfile)
